7d661dd338f0f98e37c12d8eca426666d0c45bae
AtilaRajkov <atilar777@gmail.com>
2019-02-14 19:00:56 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Git Test 1</title>
+  </head>
+  <body>
+    
+    <div>
+      <ul>
+        <li>bricks</li>
+        <li>mortar</li>
+        <li></li>
+      </ul>
+    </div>
+    
+  </body>
+</html>
